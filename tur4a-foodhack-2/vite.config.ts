2c5ad64fc0b1079f3cf761117917bec7a9db1d04
tur4aa <tur4apower@gmail.com>
2026-06-02 03:38:35 +0400

    Update vite.config.ts

--- tur4a-foodhack-2/vite.config.ts
+++ tur4a-foodhack-2/vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/tur4afoodhack/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
